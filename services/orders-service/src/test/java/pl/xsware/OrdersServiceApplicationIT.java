8c4675c87082b2a9d48af33515b967a93efe2b1b
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2026-02-03 19:02:54 +0100

    feat(orders): configure spring boot application

---
+++ services/orders-service/src/test/java/pl/xsware/OrdersServiceApplicationIT.java
@@ -0,0 +1,13 @@
+package pl.xsware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
