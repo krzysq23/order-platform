266937ef55f19a106384548cf1b9fdbb273b2fea
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2026-02-10 14:26:44 +0100

    feat: init empty inventory service project

---
+++ services/inventory-service/src/test/java/pl/xsware/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.xsware.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
